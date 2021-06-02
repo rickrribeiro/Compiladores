d6a3d24db200e20521dd8e2647d6ed7fd753c158
rickrribeiro <rickribeiro99@gmail.com>
2021-06-02 02:50:11 -0300

    finalizando regra 3 e todas as regras

--- transicoes.py
+++ transicoes.py
@@ -131,8 +131,61 @@ print(rule2)
 
 
 
-# for idx, text[i] in enumerate(text):
-#     print('rule3')
+i=0
+init = 0
+end = 0
+old_i =0 #guarda o valor de i quando ele começou a buscar no parentesis
+open=0
+aux=0    
+while i < len(text):
+    
+    if text[i] == '{':
+        i+=1
+        old_i = i
+        #vai buscar o estado final
+        while(True):
+            
+            if text[i] == '(' or  text[i]=='[' or text[i]=='{':
+                open+=1
+            if text[i] == ')' or  text[i]==']'or text[i]=='}':
+                if open == 0:
+                    
+                    while i<len(text)-1 and(ord(text[i+1]) >= ord('0') and ord(text[i+1])<= ord('9')):
+                        
+                        end*=10
+                        end+= ord(text[i+1]) - ord('0')
+                        i+=1
+                    break
+                else:
+                    open-=1 
+            i+=1               
+        i=old_i
+
+        while(open!=-1):
+            if text[i] == '(' or  text[i]=='[' or text[i]=='{':
+                open+=1
+            
+            if ((text[i]== '|' or (text[i]==')' or text[i]==']' or text[i]=='}')) and open==0):
+                aux=i
+                while(ord(text[aux-1]) >= ord('0') and ord(text[aux-1])<= ord('9')):
+                   aux-=1
+                while(ord(text[aux]) >= ord('0') and ord(text[aux])<= ord('9')):
+                    init*=10
+                    init+=ord(text[aux]) - ord('0')
+                    aux+=1    
+                rule3.append((init,'e',end))
+                init=0     
+            if text[i] == ')' or  text[i]==']' or text[i]=='}':        
+                open-=1
+            i+=1
+        i=old_i
+        end=0   
+    else:
+        i+=1
+        open=0    
+
+print(rule3)    
+
 
 
 
