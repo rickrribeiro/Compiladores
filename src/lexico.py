ef897984abd6623730b152119b57c54388497101
rickrribeiro <rickribeiro99@gmail.com>
2021-06-17 21:08:41 -0300

    estruturando a aplicação

--- src/lexico.py
+++ src/lexico.py
@@ -1,14 +1,19 @@
 from src.tabelas import tabelaSimbolos, tabelaSimbolosPalavras
 
-def lexicalAnalyzer(source, states, final):
-    result = []
-    result.append('E02 - Ricardo Ramos Ribeiro - rickribeiro99@gmail.com - 71 996820764')
+def lexicalAnalyzer(source, states):
+    
     table = tabelaSimbolosPalavras()
     #lembrar:
     #pegar o caracter i e adicionar em aux todos os simbolos e palavras reservadas que começam com ele, então ir p ir p/ i+1
     #em i+1 já descartar os que não são e passar pra i+2...i+n
     #verificar espaço apos os que começam com A pois são palavras reservadas, nos simbolos não precisa
+    #// e /* */, se for /* desconsidera td
+      # for i in source:
+    #         print(i)
+    #         if i == '\n':
+    #             print("AAAAAAAAAAAA")
+
     aux = []
     
     
-    return[result,tabelaSimbolos(None)]
+    return source
--- staticchecker.py
+++ staticchecker.py
@@ -2,6 +2,7 @@ from os import stat
 from src.lexico import lexicalAnalyzer
 from src.bota_ponto import bpontos
 import sys
+from src.tabelas import tabelaSimbolos
 
 if len(sys.argv)!= 2:
     print('Informe o codigo fonte! ex: python ./staticchecker.py entrada.201')
@@ -28,52 +29,54 @@ final = []
 # testeFinal = f.read()
 # f.close()
 
-# name = 'program'
-# f = open('txts\\wirth_'+name+'.txt', 'r')
-# text = f.read()
-# f.close()
-# programStates = bpontos(name, text)
-# f = open('txts\\estado_final_'+name+'.txt', 'r')
-# programFinal = f.read()
-# f.close()
-# states.append(programStates)
-# final.append(programFinal)
+name = 'program'
+f = open('txts\\wirth_'+name+'.txt', 'r')
+text = f.read()
+f.close()
+programStates = bpontos(name, text)
+f = open('txts\\estado_final_'+name+'.txt', 'r')
+programFinal = f.read()
+f.close()
+states.append(programStates)
+final.append(programFinal)
 
-# name = 'factor'
-# f = open('txts\\wirth_'+name+'.txt', 'r')
-# text = f.read()
-# f.close()
-# factorStates = bpontos(name, text)
-# f = open('txts\\estado_final_'+name+'.txt', 'r')
-# factorFinal = f.read()
-# f.close()
-# states.append(factorStates)
-# final.append(factorFinal)
+name = 'factor'
+f = open('txts\\wirth_'+name+'.txt', 'r')
+text = f.read()
+f.close()
+factorStates = bpontos(name, text)
+f = open('txts\\estado_final_'+name+'.txt', 'r')
+factorFinal = f.read()
+f.close()
+states.append(factorStates)
+final.append(factorFinal)
 
-# name = 'statement'
-# f = open('txts\\wirth_'+name+'.txt', 'r')
-# text = f.read()
-# f.close()
-# statementStates = bpontos(name, text)
-# f = open('txts\\estado_final_'+name+'.txt', 'r')
-# statementFinal = f.read()
-# f.close()
-# states.append(statementStates)
-# final.append(statementFinal)
+name = 'statement'
+f = open('txts\\wirth_'+name+'.txt', 'r')
+text = f.read()
+f.close()
+statementStates = bpontos(name, text)
+f = open('txts\\estado_final_'+name+'.txt', 'r')
+statementFinal = f.read()
+f.close()
+states.append(statementStates)
+final.append(statementFinal)
+
+
+while True:
+    result = lexicalAnalyzer(source,states)
+    if result == source:
+        break
+    else:
+        source = result
 
 
 
-result = lexicalAnalyzer(source,states,final)
 filename = sys.argv[1].split('.201')
-# for i in source:
-#         print(i)
-#         if i == '\n':
-#             print("AAAAAAAAAAAA")
-
 f = open(filename[0]+'.LEX', 'w+')
-f.write(str(result[0]))
+f.write(source)
 f.close
 f = open(filename[0]+'.TAB', 'w+')
-f.write(str(result[1]))
+f.write(str(tabelaSimbolos(None)))
 f.close
 
