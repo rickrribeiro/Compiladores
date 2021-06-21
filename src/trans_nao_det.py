284f7aa62ede72dcd68bea9156088e4237d610c1
brbrbrkin <36991111+brbrbrkin@users.noreply.github.com>
2021-06-21 01:31:27 -0300

    transicoes nao det?

--- src/trans_nao_det.py
+++ src/trans_nao_det.py
@@ -1,6 +1,30 @@
 from src.helpers import getState
 #lembrar:
 # verificar em todas os estados se não tem nenhuma transicao para aquele estado antes de remover algum estado final
+
+
 def eliminaNaoDeterministica(name, states):
+    for estado in states:     
+           
+        if len(estado.transicoes) > 1:
+            newEstado = states
+            newEstado.transicoes = estado.transicoes
+            newEstado.isFinal = estado.isFinal
+            newEstado.estado = states[-1].estado + 1
+            states.append(newEstado)
+            for aux in states:
+                for transaux in aux.transicoes:
+                    for fin in transaux.finais:
+                        auxEstado = getState(fin)
+                        if auxEstado == estado.estado:
+                            auxEstado.transicoes
+                            for tempTrans in auxEstado.transicoes:
+                                for finTemp in tempTrans.finais:
+                                    if finTemp == estado.estado:
+                                        finTemp = newEstado.estado
+            states.remove(estado)
     
     return states
+
+
+   
